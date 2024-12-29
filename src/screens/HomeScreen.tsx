1de2d8465c67012349109e2ea742648f6408c105
Ayaan Ahmad <ayaansheikh061@gmail.com>
2024-12-30 04:45:19 +0500

    bug fixed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,37 +1,9 @@
-// import React from 'react'
-// import Header from '../components/Header'
-// import Background from '../components/Background'
-// import Footer from '../components/Footer'
-// import Recents from '../components/Recents'
-// import SurahList from '../components/SurahList'
-
-// import { NavigationProp } from '@react-navigation/native';
-
-// interface HomeScreenProps {
-//   navigation: NavigationProp<any>;
-// }
-
-// export default function HomeScreen({ navigation }: HomeScreenProps) {
-//   return (
-//     <>
-//       <Background />
-//       <Header />
-//       <Recents />
-//       <SurahList navigation={navigation} />
-//       <Footer />
-//     </>
-//   )
-// }
-
-
-
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import SurahList from '../components/SurahList';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/Types';
-import Recents from '../components/Recents';
 import Footer from '../components/Footer';
 import Background from '../components/Background';
 
@@ -48,7 +20,6 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
     <View style={styles.container}>
       <Background />
       <Header title="Search Surah" onSearch={handleSearch} />
-      <Recents />
       <SurahList navigation={navigation} searchQuery={searchQuery} />
       <Footer navigation={navigation} />
     </View>
